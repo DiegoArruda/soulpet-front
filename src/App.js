c48e076925949c01a10a7f6747237a9be36bdbdc
GuLeonardi <gustavo.leonardi01@gmail.com>
2023-05-04 16:17:04 -0300

    criação de editar pedido

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import { NovoAgendamento } from "./pages/NovoAgendamento/NovoAgendamento";
 import { Servicos } from "./pages/Servicos/Servicos";
 import { Produtos } from "./pages/Produtos/Produtos";
 import { Agendamento } from "./pages/Agendamento/Agendamento";
+import { EditaPedido } from "./pages/EditaPedido/editaPedido";
 
 
 
@@ -40,7 +41,7 @@ function App() {
           <Route path="/pedidos/novo" element={<NovoPedido/>} /> 
           <Route path="/produtos" element={<Produtos />} />
           <Route path="/produtos/novo" element={<NovoProduto />} />
-          
+          <Route path="/pedidos/editar/:id" element={<EditaPedido/>} />
           <Route path="/agendamentos" element={<Agendamento />} />
           <Route path="/agendamentos/novo" element={<NovoAgendamento />} />
         </Route>
